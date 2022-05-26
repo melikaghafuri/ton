065ea2739303c225182db55037504a702b76a72b
Melika Ghafuri <melikaghafuri@gmail.com>
2022-05-26 15:55:33 +0430

    "transactions counter ^.^ "

---
+++ syntax.js
@@ -0,0 +1,8 @@
+function transactionsFuncion() {
+    let transactionsCounter = document.getElementById("transactions");
+    let transactionsValue = Number(transactionsCounter.innerHTML);
+    transactionsValue = transactionsValue + 1;
+    transactionsCounter.innerHTML = transactionsValue;
+}
+
+setInterval(transactionsFuncion, 500);
